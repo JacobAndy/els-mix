dc53f450c234be91b4913b8f01abf0c746213a6d
Jacob Anderson <odistiinct@gmail.com>
2018-08-28 17:58:10 -0500

    set up auth 2.0

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const express = require("express"),
   massive = require("massive"),
   PORT = 9001,
   passport = require("passport"),
-  { strategy } = require("./controllers/auth0");
+  strategy = require("./controllers/strategy"),
+  flash = require("connect-flash");
 
 app.use(express.json());
 app.use(cors());
+app.use(flash());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -22,9 +24,20 @@ app.use(
   })
 );
 
-passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  console.log("ser");
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deser");
+  done(null, obj);
+});
 
 massive(process.env.DATABASE_URL)
   .then(db => {
@@ -32,5 +45,25 @@ massive(process.env.DATABASE_URL)
     app.set("db", db);
   })
   .catch(err => console.log(err));
+
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "http://localhost:9001/me",
+    failureRedirect: "http://localhost:9001/me",
+    failureFlash: true
+  })
+);
+app.get("/me", (req, res, next) => {
+  res.status(200).json("hello world");
+  // if (!req.user) {
+  //   res.redirect("/login");
+  // } else {
+  //   // req.user === req.session.passport.user
+  //   // console.log( req.user )
+  //   // console.log( req.session.passport.user );
+  //   res.status(200).send(JSON.stringify(req.user, null, 10));
+  // }
+});
 
 app.listen(PORT, () => console.log(`${PORT}, Shelby Drive Look Alive`));
